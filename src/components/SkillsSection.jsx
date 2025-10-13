ee82bdb87419278ca9dfc7035cf3a54b67f531d5
David Polo Abrugena <dpbabrugena@gmail.com>
2025-10-13 18:56:09 +0800

    2025-10-13 v.04: Updated the styling for skills section

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -78,7 +78,7 @@ export const SkillsSection = () => {
                             key={key}
                             onClick={() => setActiveCategory(category)}
                             className={cn(
-                                "px-5 py-2 rounded-full transition-colors duration-300 capitalize",
+                                "px-5 py-2 rounded-full transition-colors duration-300 capitalize cursor-pointer",
                                 (activeCategory === category) ? "bg-primary text-primary-foreground" : "bg-secondary/70 text-foreground hover:bg-secondary"
                             )}
                         >
