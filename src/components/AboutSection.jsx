776974a98c8f79f9b772b61e7dcaeec00334fad2
David Polo Abrugena <dpbabrugena@gmail.com>
2025-12-09 18:04:55 +0800

    2025-12-09 v.01: Updated the resume link

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -31,7 +31,7 @@ export const AboutSection = () => {
 
                         <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
                             <a 
-                                href="https://drive.google.com/file/d/1RyfFbCOCYxGytnUIPfDlFSyqsn-InNyz/view?usp=sharing" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
+                                href="https://drive.google.com/file/d/1IeYMIQN0O9LWOt9ndpBt7Mm69XTZFLUK/view?usp=sharing" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
                             >
                                 {" "}
                                 Download Resume
