c96fff0a24565664e6435f40146bfad5ab04c625
David Polo Abrugena <dpbabrugena@gmail.com>
2025-12-10 18:34:00 +0800

    2025-12-10 v.01: Updated the About Section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -14,7 +14,7 @@ export const AboutSection = () => {
                     {/* Quote Section */}
                     <div className="space-y-6">
                         <h3 className="text-2xl font-semibold">Passionate Web Developer, Engineer & IT Tech Support</h3>
-                        
+
                         <p className="text-muted-foreground">
                             Despite the differences between my previous career as a professional chemical engineer, I successfully transitioned into my first role in tech, where I was responsible for addressing technical issues faced by clients using a web-based application. This application is a Software as a Service (SaaS) platform that functions as a Laboratory Information System (LIS), managing medical records and transactions for various medical institutions across the Philippines.
                         </p>
@@ -30,8 +30,8 @@ export const AboutSection = () => {
                         </div>
 
                         <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
-                            <a 
-                                href="https://drive.google.com/file/d/1IeYMIQN0O9LWOt9ndpBt7Mm69XTZFLUK/view?usp=sharing" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
+                            <a
+                                href="https://drive.google.com/file/d/1HC4I2hh0iAy41ov5XGDRywZW353kT27u/view?usp=sharing" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
                             >
                                 {" "}
                                 Download Resume
@@ -48,11 +48,11 @@ export const AboutSection = () => {
 
                     {/* Skills Section */}
                     <div className="grid grid-cols-1 gap-6">
-                        
+
                         <div className="gradient-border p-6 card-hover">
                             <div className="flex items-start gap-4">
                                 <div className="p-3 rounded-full bg-primary/10">
-                                    <Headset className="h-6 w-6 text-primary"/>
+                                    <Headset className="h-6 w-6 text-primary" />
                                 </div>
 
                                 <div className="text-left">
@@ -65,7 +65,7 @@ export const AboutSection = () => {
                         <div className="gradient-border p-6 card-hover">
                             <div className="flex items-start gap-4">
                                 <div className="p-3 rounded-full bg-primary/10">
-                                    <Code className="h-6 w-6 text-primary"/>
+                                    <Code className="h-6 w-6 text-primary" />
                                 </div>
 
                                 <div className="text-left">
@@ -78,7 +78,7 @@ export const AboutSection = () => {
                         <div className="gradient-border p-6 card-hover">
                             <div className="flex items-start gap-4">
                                 <div className="p-3 rounded-full bg-primary/10">
-                                    <UsersRound className="h-6 w-6 text-primary"/>
+                                    <UsersRound className="h-6 w-6 text-primary" />
                                 </div>
 
                                 <div className="text-left">
